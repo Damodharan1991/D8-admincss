2154569687f65a2fc5ef82bcc242d218b7eda9fc
Damodharan K <Damodharan1991@3308527.no-reply.drupal.org>
2018-08-28 19:52:38 +0530

    added source

--- README.txt
+++ README.txt
@@ -1,1 +1,46 @@
-# admincss
+INTRODUCTION
+
+ * The Admin CSS module allows site builders to add CSS through the admin
+ dashboard. For a full description of the module please visit: 
+ https://www.drupal.org/project/admincss.
+
+
+REQUIREMENTS
+
+ * This module does not requires the any dependency modules
+
+
+INSTALLATION
+
+ * Install the Admin CSS module as you would normally install a contributed
+   Drupal module. Visit https://www.drupal.org/node/1897420 for further
+   information.
+
+CONFIGURATION
+
+ * Navigate to Administration > Extend and enable the module.
+
+ * Navigate to Administration » config » people » admincss to add custom css.
+
+ * Save your configuration. When enabled, the module will allows users to 
+   add css in admin dashboard.
+
+ * To remove the Admin CSS, disable the module and clear caches.
+
+ 
+MAINTAINERS
+
+Current maintainer:
+
+ * Damodharan (damu) - https://www.drupal.org/user/3308527
+
+This project has been sponsored by:
+
+ * Pepper Square Software Services Private Limited specialized in software 
+   product development and planning of Drupal powered sites.
+
+ * Pepper Square offers complete development cycle and hosting to get you start. 
+   Visit: https://www.peppersquare.com for more information.
+ 
+ * To submit bug reports and feature suggestions, or to track changes visit:
+   https://www.drupal.org/project/issues/admincss
---
+++ src/Form/SimpleForm.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Drupal\admin_css\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * SimpleForm with some custom functions and settings.
+ */
+
+class SimpleForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'admin_css_config_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function getEditableConfigNames() {
+    return [
+      'admin_css.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  
+  public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
+    $form['custom_css'] = array(
+     '#type' => 'textarea',
+     '#title' => $this->t('Enter your custom css'),
+     '#default_value' => $this->config('admin_css.settings')->get('custom_css'),
+     '#rows' => 10,
+   );
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+  * Form submission handler.
+  */
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $userInputValues = $form_state->getUserInput();
+    $config = $this->configFactory->getEditable('admin_css.settings');
+    $config->set('custom_css', $form_state->getValue('custom_css'));
+    $config->save();
+    $cur_data = $this->config('admin_css.settings')->get('custom_css');
+    $file = file_save_data($cur_data, "public://admin-style.css", FILE_EXISTS_REPLACE);
+    parent::submitForm($form, $form_state);
+
+  }
+
+}
